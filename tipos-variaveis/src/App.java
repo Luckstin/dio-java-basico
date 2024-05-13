7129c4e5909e22947ad0099dbd6b606dc88e5197
Lucas <lucasoliveira.tec@gmail.com>
2024-05-13 19:48:37 -0300

    Variaveis e operadores Java

---
+++ tipos-variaveis/src/App.java
@@ -0,0 +1,14 @@
+/**
+ * Existem 8 tipos primitivos de variáveis que são: int, byte
+ * short, long, float, double(mais utilizado), boolean, e char;
+ * 
+ * 
+ * 
+ */
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ tipos-variaveis/src/TipoDados.java
@@ -0,0 +1,14 @@
+public class TipoDados {
+        public static void main (String[] args){
+                byte idade = 123;
+                short ano = 2021;
+                int cep = 21070333; //melhor utilizar a estrutura abaixo
+                long cpf = 1234589101L; //precisa do L ao final para armz.
+                String cep0 = ("02107033");
+                float pi = 3.14F; //sempre precisa do F ao final
+                double salario = 1275.33;//.33 quer dizer centavos
+
+        }
+
+
+}
